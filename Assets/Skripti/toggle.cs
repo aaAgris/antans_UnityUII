5214aa9fdcf184a0910b1a16fa95ce1ce5940e32
Antans <antansagris@gmail.com>
2022-02-23 15:36:15 +0200

    toggle funkcija

---
+++ Assets/Skripti/toggle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggle : MonoBehaviour {
+
+	public GameObject dzelzsCap;
+
+	public void dzelzsCapAttelosana(bool vertiba){
+		dzelzsCap.SetActive (vertiba);
+	}
+
+}

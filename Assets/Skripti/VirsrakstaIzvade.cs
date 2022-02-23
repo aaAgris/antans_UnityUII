9e5f72fa37055c87426e1ef101578315cebb35c9
Antans <antansagris@gmail.com>
2022-02-23 12:10:35 +0200

    pogas izveide

---
+++ Assets/Skripti/VirsrakstaIzvade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirsrakstaIzvade : MonoBehaviour {
+
+	public string vards;
+	public string gadi;
+	public GameObject ievadeVards;
+	public GameObject ievadeGadi;
+	public GameObject tekstaAttelosana;
+
+	public void uzglabaTekstu(){
+		vards = ievadeVards.GetComponent<Text> ().text;
+		gadi = ievadeGadi.GetComponent<Text> ().text;
+		tekstaAttelosana.GetComponent<Text> ().text = "Varonim ir " + gadi+" gadi un vińu sauc "+vards+ "!";
+
+	}
+
+}
